92ab8d074a0e8b57b2ab6fb2336deb804658e7cb
TC-Penrose <106706046+TC-Penrose@users.noreply.github.com>
2022-06-01 15:36:41 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -77,8 +77,9 @@ forever(function () {
             Bobbetita.y += -1.4
         }
         if (Bobbetita.y <= 78) {
+            Bobbetita.y += Normal_gravity
             Normal_gravity = -1
-            pause(200)
+            pause(1000)
             Normal_gravity = 1
         }
     }
@@ -94,9 +95,23 @@ forever(function () {
     }
 })
 forever(function () {
-    if (current_tilemap == "Swamp tile-map") {
-        if (Bobbetita.y <= 78) {
+    if (current_tilemap == "BossBattle") {
+        if (Bobbetita.y < 72) {
             Bobbetita.y += Normal_gravity
+        }
+        if (Bobbetita.y > 78) {
+            Bobbetita.y += UnderwaterGravity
+        }
+    }
+})
+forever(function () {
+    if (current_tilemap == "Swamp tile-map") {
+        if (Bobbetita.y < 77) {
+            Bobbetita.y += Normal_gravity
+        }
+        if ((Bobbetita.y == 77 || Bobbetita.y == 78) && !(controller.up.isPressed())) {
+            Bobbetita.y += Normal_gravity
+            Normal_gravity = 0
         }
         if (Bobbetita.y > 78) {
             Bobbetita.y += UnderwaterGravity
