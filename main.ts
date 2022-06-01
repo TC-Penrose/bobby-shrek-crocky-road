3fbbfaa5bcad33434ca1593e8cc2c715f18eaedd
TC-Penrose <106706046+TC-Penrose@users.noreply.github.com>
2022-06-01 15:48:21 -0700

    Gravity works, making the jump

--- main.ts
+++ main.ts
@@ -73,14 +73,11 @@ forever(function () {
 })
 forever(function () {
     if (controller.up.isPressed()) {
-        if (Bobbetita.y > 78) {
+        if (Bobbetita.y >= 78) {
             Bobbetita.y += -1.4
         }
-        if (Bobbetita.y <= 78) {
-            Normal_gravity = -1
-            Bobbetita.y += Normal_gravity
-            pause(1000)
-            Normal_gravity = 1
+        if (Bobbetita.y < 78) {
+            Bobbetita.y += -2
         }
     }
 })
@@ -106,8 +103,12 @@ forever(function () {
 })
 forever(function () {
     if (current_tilemap == "Swamp tile-map") {
-        if (Bobbetita.y < 78) {
+        if (Bobbetita.y < 77) {
             Bobbetita.y += Normal_gravity
+        }
+        if ((Bobbetita.y == 77 || Bobbetita.y == 78) && !(controller.up.isPressed())) {
+            Bobbetita.y += Normal_gravity
+            Normal_gravity = 0
         }
         if (Bobbetita.y > 78) {
             Bobbetita.y += UnderwaterGravity
