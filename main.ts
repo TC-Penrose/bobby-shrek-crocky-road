98fa088e78763d4ed7ea432d2e4cd23b05e18b3c
TC-Penrose <106706046+TC-Penrose@users.noreply.github.com>
2022-06-02 16:56:04 -0700

    used an array to progressively increase the boss speed

--- main.ts
+++ main.ts
@@ -3,19 +3,29 @@ namespace SpriteKind {
     export const abyss = SpriteKind.create()
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile13`, function (sprite, location) {
-    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
     tiles.setCurrentTilemap(tilemap`BossBattle`)
     current_tilemap = "BossBattle"
     tiles.placeOnTile(Bobbetita, tiles.getTileLocation(18, 1))
     tiles.placeOnTile(Onion, tiles.getTileLocation(17, 2))
-    Lvl2_arrowCounter = []
+    sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+    SpawnGator(60, 80, 0, "2")
+    Big_Boss_Move()
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile16`, function (sprite, location) {
+    if (true) {
+        scene.setBackgroundColor(11)
+        current_tilemap = "level3"
+        tiles.placeOnTile(Bobbetita, tiles.getTileLocation(18, 1))
+        sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
+        sprites.destroyAllSpritesOfKind(SpriteKind.Food)
+    }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
     if (current_tilemap == "Swamp tile-map") {
-        Lifescore += randint(1, 5)
+        Score += randint(1, 5)
         Onion.setPosition(randint(0, 1000), randint(0, 35))
     } else if (current_tilemap == "BossBattle") {
-        Lifescore += randint(1, 5)
+        Score += randint(1, 5)
         Onion.destroy()
         pause(5000)
         Onion = sprites.create(assets.image`Onion`, SpriteKind.Food)
@@ -23,6 +33,18 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSpr
         tiles.placeOnTile(Onion, tiles.getTileLocation(17, 2))
     }
 })
+function Big_Boss_Move () {
+    Gator.setBounceOnWall(true)
+    Gator.ax = 3
+    Gator.ay = 3
+    gatorMovementLvl_2 = ["run faster"]
+    for (let value of gatorMovementLvl_2) {
+        pause(100)
+        gatorMovementLvl_2.push("run faster")
+        Gator.ax += 3
+        Gator.ay += 3
+    }
+}
 function SpawnGator (xSpawn: number, ySpawn: number, Interval: number, level: string) {
     if (level == "1") {
         pause(Interval)
@@ -40,18 +62,13 @@ function SpawnGator (xSpawn: number, ySpawn: number, Interval: number, level: st
     }
 }
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
-    if (current_tilemap == "Swamp tile-map") {
-        otherSprite.destroy()
-    }
-    if (current_tilemap == "BossBattle") {
-        Lvl2_arrowCounter.push("Boss hit!")
-    }
+    otherSprite.destroy(effects.disintegrate, 500)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     scene.cameraShake(4, 500)
-    Lifescore += -1
+    Score += -1
 })
-let Lvl2_arrowCounter: string[] = []
+let gatorMovementLvl_2: string[] = []
 let Gator: Sprite = null
 let Onion: Sprite = null
 let Bobbetita: Sprite = null
@@ -70,9 +87,9 @@ tiles.placeOnTile(Bobbetita, tiles.getTileLocation(7, 7))
 Bobbetita.setScale(2, ScaleAnchor.Middle)
 Bobbetita.sayText("GET OUT OF MY SWAMP.", 2000, true)
 pause(2000)
-let Lifescore = 5
-let textSprite = textsprite.create(convertToText(Lifescore))
-textSprite.setScale(1.5, ScaleAnchor.Middle)
+let Score = 5
+let Score_text = textsprite.create(convertToText(Score))
+Score_text.setScale(1.5, ScaleAnchor.Middle)
 let UnderwaterGravity = 0.5
 let Normal_gravity = 1
 Bobbetita.setScale(1, ScaleAnchor.Middle)
@@ -87,7 +104,7 @@ forever(function () {
     }
 })
 forever(function () {
-    if (controller.A.isPressed()) {
+    if (controller.B.isPressed()) {
         Arrow = sprites.createProjectileFromSprite(assets.image`Arrow_right`, Bobbetita, 50, 10)
     }
 })
@@ -102,7 +119,7 @@ forever(function () {
     }
 })
 forever(function () {
-    if (controller.B.isPressed()) {
+    if (controller.A.isPressed()) {
         Arrow = sprites.createProjectileFromSprite(assets.image`Arrow_Left`, Bobbetita, -50, 10)
     }
 })
@@ -127,10 +144,8 @@ forever(function () {
     }
 })
 forever(function () {
-    textSprite.setText(convertToText(Lifescore))
-})
-forever(function () {
-    textSprite.setPosition(scene.cameraProperty(CameraProperty.Left) + 15, scene.cameraProperty(CameraProperty.Top) + 15)
+    Score_text.setText(convertToText(Score))
+    Score_text.setPosition(scene.cameraProperty(CameraProperty.Left) + 15, scene.cameraProperty(CameraProperty.Top) + 15)
 })
 forever(function () {
     if (current_tilemap == "Swamp tile-map") {
@@ -151,12 +166,6 @@ forever(function () {
     if (current_tilemap == "Swamp tile-map") {
         SpawnGator(0, randint(80, 300), 1000, "1")
     }
-    if (current_tilemap == "BossBattle") {
-        sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
-        SpawnGator(60, 80, 0, "2")
-        Gator.setVelocity(15, -14)
-        Gator.setBounceOnWall(true)
-    }
 })
 forever(function () {
     if (current_tilemap == "BossBattle") {
@@ -169,9 +178,10 @@ forever(function () {
     }
 })
 forever(function () {
-    if (Lvl2_arrowCounter.length == 5) {
-        sprites.destroyAllSpritesOfKind(SpriteKind.Enemy)
-    }
+	
+})
+forever(function () {
+	
 })
 forever(function () {
 	
